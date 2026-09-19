d4a58405b5fa8e33df8a1e39eed5aed527402d38
yupix <yupi0982@outlook.jp>
2026-09-19 23:50:49 +0900

    fix: アイコンの xmlns でドキュメントの hydration がずれる
    Starlight の Tabs はスロットの HTML を hast へ読み直して組み直すため、インライン
    SVG の xmlns が :xmlns という別の属性になり、Vue・React とも描画後に属性が
    食い違っていた。インライン SVG に xmlns は不要なので 5 つのアイコンから外す。

--- components/EllipsisIcon/EllipsisIcon.lite.tsx
+++ components/EllipsisIcon/EllipsisIcon.lite.tsx
@@ -1,4 +1,5 @@
 // Lucide ellipsis; ISC / Feather MIT. See THIRD_PARTY_NOTICES.md.
+// インライン SVG に xmlns は不要。付けると Starlight の Tabs が HTML を組み直すときに :xmlns へ変わり、hydration がずれる。
 export interface EllipsisIconProps {
   size?: number;
 }
@@ -6,7 +7,6 @@ export interface EllipsisIconProps {
 export default function EllipsisIcon(props: EllipsisIconProps) {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
       width={props.size ?? 16}
       height={props.size ?? 16}
       viewBox="0 0 24 24"
